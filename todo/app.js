5b2447e3a2708ee06b1c035ba11f6856ff8a6a47
Neha_Kumari711 <nk8981398@gmail.com>
2024-05-07 11:46:14 +0530

    added features

---
+++ todo/app.js
@@ -0,0 +1,17 @@
+let fieldset= document.querySelector(".todo-list");
+let labels= document.querySelector(".todo-list__label");
+let span= document.querySelector("span");
+
+let btn= document.querySelector(".btn-input");
+
+btn.addEventListener("click", ()=>{
+    // console.log("button clicked");
+    // addTask();
+});
+
+function addTask(){
+    // let label=document.createElement("");
+    let input= document.querySelector(".task-input").value;
+    
+    
+}
